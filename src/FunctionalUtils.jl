2d2fcd650342561c255d8490c1471a5383941cb9
Zachary Allaun <zach.allaun@gmail.com>
2013-04-30 14:49:19 -0400

    Fun => FunctionalUtils

---
+++ src/FunctionalUtils.jl
@@ -0,0 +1,321 @@
+module FunctionalUtils
+
+export # Basic functions
+       partial,
+       rpartial,
+       not,
+       complement,
+       inc,
+       dec,
+       halve,
+       explode,
+       implode,
+       truthy,
+       falsey,
+       ispos,
+       isneg,
+       iszero,
+
+       # Sequence operations
+       cons,
+       construct,
+       rest,
+       butlast,
+       drop,
+       takewhile,
+       dropwhile,
+       partition,
+       partitionall,
+       mapcat,
+       interpose,
+       mapkeys,
+       mapvals,
+       interleave,
+       repeat,
+       repeatedly,
+       cycle,
+       splitat,
+
+       # Combinators
+       constantly,
+       always,
+       pipeline,
+       comp,
+       everypred,
+       somepred,
+       splat,
+       unsplat,
+
+       # Applicative functions
+       update,
+       remove,
+       accessor,
+       first,
+       second,
+       third,
+       juxt,
+       iterate,
+       iterateuntil,
+       reductions,
+
+       # HOFs
+       best,
+       fnothing,
+       curry2,
+       curry3,
+       curry4,
+       gt,
+       gte,
+       lt,
+       lte,
+       eq,
+       trampoline,
+       Continue,
+       @jump
+
+# Basic functions
+# ===============
+
+partial(f::Function, args1...) = (args2...) -> f(args1..., args2...)
+rpartial(f::Function, args1...) = (args2...) -> f(args2..., reverse(args1)...)
+
+complement = not(f::Function) = (args...) -> !f(args...)
+
+inc(n) = n + 1
+dec(n) = n - 1
+halve(n) = n / 2
+
+explode(s::String) = [s...]
+implode(cs) = apply(string, cs)
+
+truthy(val) = !is(val, nothing) && !is(val, false)
+falsey(val) = !truthy(val)
+
+ispos(n)  = n >= 0
+isneg(n)  = n < 0
+iszero(n) = n == 0
+
+# Sequence operations
+# ===================
+
+Base.convert{T}(::Type{Vector{T}}, t::Tuple) = T[e for e in t]
+Base.convert(::Type{Vector}, t::Tuple) = [e for e in t]
+
+cons = construct(head, tail) = vcat([head], convert(Vector, tail))
+
+rest(seq) = seq[2:end]
+butlast(seq) = seq[1:end-1]
+
+Base.take(n::Int, seq) = seq[1:n]
+drop(n::Int, seq) = seq[n+1:end]
+
+function indexwhere(pred, seq)
+    idx = 0
+    for i=1:length(seq)
+        !pred(seq[i]) ? idx = i : return idx
+    end
+    idx
+end
+takewhile(pred, seq) = seq[1:indexwhere(not(pred), seq)]
+dropwhile(pred, seq) = seq[indexwhere(not(pred), seq)+1:end]
+
+function partition(n, coll; all=false)
+    if length(coll) >= n
+        front, back = 1, n
+        partitioned = typeof(coll[front:back])[]
+        while back <= length(coll)
+            push!(partitioned, coll[front:back])
+            front, back = back+1, back+n
+        end
+        all && push!(partitioned, coll[front:end])
+        partitioned
+    else
+        all ? [coll] : Any[]
+    end
+end
+partitionall(n, coll) = partition(n, coll, all=true)
+
+mapcat(f, args...) = apply(vcat, map(f, args...))
+
+interpose(inter, seq) = butlast(mapcat((e) -> [e, inter], seq))
+
+Base.map(f, z::Zip) = [f(tup) for tup in z]
+function Base.map{K, V}(f, dict::Dict{K, V})
+    d = Dict{K, V}()
+    for (k, v) in dict
+        k, v = f(k, v)
+        d[k] = v
+    end
+    d
+end
+
+mapkeys(f, d::Dict) = map((k, v) -> (f(k), v), d)
+mapvals(f, d::Dict) = map((k, v) -> (k, f(v)), d)
+
+interleave(seqs...) = mapcat(partial(convert, Vector), zip(seqs...))
+
+repeat(n, val) = [val for _ in 1:n]
+repeatedly(n, f) = [f() for _ in 1:n]
+
+cycle(n, elements) = take(n, mapcat(_ -> elements, 1:int(n/length(elements))))
+
+splitat(i, seq) = (seq[1:i-1], seq[i:end])
+
+# TODO:
+# increasing
+# decreasing
+# increasingoreq
+# decreasingoreq
+# dispatcher
+# takeskipping
+
+# Combinators
+# ===========
+
+always = constantly(val) = (_...) -> val
+
+pipeline(seed, fs::Function...) = reduce((val, f) -> f(val), seed, fs)
+
+function comp(fs::Function...)
+    f = fs[end]
+    fs = reverse(butlast(fs))
+
+    function (args...)
+        chain = cons(f(args...), fs)
+        pipeline(chain...)
+    end
+end
+
+import Base.*
+*(f1::Function, f2::Function) = (args...) -> f1(f2(args...))
+
+function everypred(preds::Function...)
+    function (args...)
+        for pred in preds
+            pred(args...) || return false
+        end
+        return true
+    end
+end
+
+function somepred(preds::Function...)
+    function (args...)
+        for pred in preds
+            pred(args...) && return true
+        end
+        return false
+    end
+end
+
+splat(f::Function) = (args) -> f(args...)
+unsplat(f::Function) = (args...) -> f(args)
+
+# Applicative functions
+# =====================
+
+function update{K, V}(d::Dict{K, V}, ks::Vector{K}, f::Function)
+    lastkey = ks[end]
+    ks = butlast(ks)
+    ret = copy(d)
+    target = ret
+
+    for k in ks
+        target = target[k]
+    end
+
+    target[lastkey] = f(target[lastkey])
+    ret
+end
+update{K, V}(d::Dict{K, V}, ks::Vector{K}, val) = update(d, ks, always(val))
+update{K, V}(d::Dict{K, V}, k::K, f) = update(d, [k], f)
+
+remove(pred, seq) = filter(not(pred), seq)
+
+accessor(key) = (coll) -> coll[key]
+first = accessor(1)
+second = accessor(2)
+third = accessor(3)
+
+juxt(fs::Function...) = (args...) -> [f(args...) for f in fs]
+
+function iterate(n, f, seed)
+    ret = Any[]
+    result = f(seed)
+
+    for _=1:n
+        push!(ret, result)
+        result = f(result)
+    end
+
+    ret
+end
+function iterateuntil(pred, f, seed)
+    ret = Any[]
+    result = f(seed)
+
+    while !pred(result)
+        push!(ret, result)
+        result = f(result)
+    end
+
+    ret
+end
+
+function reductions(f, init, seq)
+    ret = Any[init]
+    acc = init
+
+    for el in seq
+        acc = f(acc, el)
+        push!(ret, acc)
+    end
+
+    ret
+end
+reductions(f, seq) = isempty(seq) ? Any[] : reductions(f, seq[1], seq[2:end])
+
+# TODO:
+# partitionby
+
+# Higher order functions
+# ======================
+
+best(f, seq) = reduce(seq) do x, y
+    f(x, y) ? x : y
+end
+
+function fnothing(f, defaults...)
+    function (args...)
+        args = map(zip(args, defaults)) do tup
+            arg, default = tup
+            truthy(arg) ? arg : default
+        end
+        f(args...)
+    end
+end
+
+curry2(f) = last -> (first -> f(first, last))
+curry3(f) = last -> (middle -> (first -> f(first, middle, last)))
+curry4(f) = fourth -> (third -> (second -> (first -> f(first, second, third, fourth))))
+
+gt  = curry2(>)
+gte = curry2(>=)
+lt  = curry2(<)
+lte = curry2(<=)
+eq  = curry2(==)
+
+immutable Continue
+    thunk
+end
+macro jump(ex)
+    :(Continue(() -> $(esc(ex))))
+end
+function trampoline(f, args...)
+    res = f(args...)
+    while isa(res, Continue)
+        res = res.thunk()
+    end
+    res
+end
+
+end # module FunctionalUtils
---
+++ test/FunctionalUtilsTest.jl
@@ -0,0 +1,230 @@
+using FactCheck
+using FunctionalUtils
+
+@facts "Basics" begin
+
+    @fact "partial, rpartial" begin
+        add(x, y) = x + y
+        partial(add, 1)(1) => 2
+        partial(add, 1, 2, 3)() => :throws
+
+        rpartial(string, "foo")("bar") => "barfoo"
+        rpartial(string, "foo", "bar")("baz") => "bazbarfoo"
+    end
+
+    @fact "not, complement" begin
+        FunctionalUtils.not => exactly(complement)
+        FunctionalUtils.not(iseven)(3) => true
+        FunctionalUtils.not(iseven)(4) => false
+    end
+
+    @fact "truthiness" begin
+        1    => FunctionalUtils.truthy
+        0    => FunctionalUtils.truthy
+        true => FunctionalUtils.truthy
+        nothing => FunctionalUtils.falsey
+        false   => FunctionalUtils.falsey
+    end
+
+end
+
+@facts "Sequence operations" begin
+
+    @fact "explode, implode" begin
+        explode("foo") => ['f', 'o', 'o']
+        implode(['f', 'o', 'o']) => "foo"
+    end
+
+    @fact "cons, construct" begin
+        cons => exactly(construct)
+        cons(1, [2,3]) => [1, 2, 3]
+        cons(1, (2,3)) => [1, 2, 3]
+    end
+
+    @fact "butlast" begin
+        butlast([1,2,3]) => [1, 2]
+    end
+
+    @fact "take, drop" begin
+        take(3, "foobar") => "foo"
+        take(1, [1,2,3]) => [1]
+        take(1, []) => :throws
+
+        drop(3, "foobar") => "bar"
+        drop(1, [1,2,3]) => [2,3]
+        drop(1, []) => []
+    end
+
+    @fact "takewhile, dropwhile" begin
+        takewhile(iseven, []) => []
+        dropwhile(iseven, []) => []
+
+        takewhile(iseven, [2,2,2,3,2,2,2]) => [2,2,2]
+        dropwhile(iseven, [2,2,2,3,2,2,2]) => [3,2,2,2]
+
+        takewhile((c) -> c == 'x', "xxxyyy") => "xxx"
+        dropwhile((c) -> c == 'x', "xxxyyy") => "yyy"
+    end
+
+    @fact "partition, partitionall" begin
+        partition(2, [1,2,3,4]) => Array{Int}[Int[1,2], Int[3,4]]
+        partition(2, [1,2,3])   => Array{Int}[Int[1,2]]
+        partition(2, [1])       => []
+        partition(3, "foobarbaz") => ["foo", "bar", "baz"]
+
+        partitionall(2, [1,2,3]) => Array{Int}[Int[1,2], Int[3]]
+        partitionall(2, [1]) => [[1]]
+    end
+
+    @fact "mapcat" begin
+        mapcat((x) -> [x], [1,2,3]) => [1,2,3]
+    end
+
+    @fact "interpose" begin
+        interpose(10, [1,2,3]) => [1,10,2,10,3]
+        interpose(10, []) => []
+    end
+
+    @fact "interleave" begin
+        implode(interleave("foo", "bar", "baz")) => "fbboaaorz"
+        interleave([1,3], [2,4]) => [1,2,3,4]
+        interleave([1,3], [2]) => [1,2]
+    end
+
+    @fact "repeat, repeatedly" begin
+        repeat(5, 1)  => [1,1,1,1,1]
+        repeat(-1, 0) => []
+        repeat(0, 0)  => []
+
+        repeatedly(5, ()->1) => [1,1,1,1,1]
+    end
+
+    @fact "cycle" begin
+        cycle(5, [1,2,3]) => [1,2,3,1,2]
+    end
+
+    @fact "splitat" begin
+        splitat(4, "foobar") => ("foo", "bar")
+        splitat(1, "foobar") => ("", "foobar")
+    end
+
+    @fact "map(::Function, ::Dict), mapkeys, mapvals" begin
+        map((k, v) -> (k, v+1), [:x => 1, :y => 2]) => [:x => 2, :y => 3]
+        mapkeys(inc, [1 => 1, 2 => 2]) => [2 => 1, 3 => 2]
+        mapvals(inc, [1 => 1, 2 => 2]) => [1 => 2, 2 => 3]
+    end
+
+end
+
+@facts "Combinators" begin
+
+    @fact "constantly, always" begin
+        constantly => exactly(always)
+        always(5)() => 5
+        always(always)() => exactly(always)
+    end
+
+    @fact "pipeline" begin
+        pipeline(1, inc, inc, inc) => 4
+        pipeline(1) => 1
+    end
+
+    @fact "comp, *(::Function ::Function)" begin
+        comp(inc, inc, inc)(1) => 4
+        (inc * inc * inc)(1)   => 4
+    end
+
+    @fact "everypred, somepred" begin
+        everypred(ispos, iseven)(4) => true
+        everypred(ispos, iseven)(-4) => false
+
+        somepred(ispos, iseven)(-4) => true
+        somepred(ispos, isodd)(-4) => false
+    end
+
+    @fact "splat, unsplat" begin
+        foo(x, y, z) = x + y + z
+        splat(foo)([1, 2, 3]) => 6
+        unsplat(splat(foo))(1, 2, 3) => 6
+    end
+
+end
+
+@facts "Applicative functions" begin
+
+    @fact "update" begin
+        d = [:x => 1, :y => [:z => 1]]
+        update(d, :x, inc) => [:x => 2, :y => [:z => 1]]
+        update(d, :x, 2)   => [:x => 2, :y => [:z => 1]]
+        update(d, [:y, :z], inc) => [:x => 1, :y => [:z => 2]]
+        update(d, [:y, :z], 2)   => [:x => 1, :y => [:z => 2]]
+    end
+
+    @fact "remove" begin
+        remove(iseven, [1,2,3,4,5,6]) => [1,3,5]
+    end
+
+    @fact "accessor" begin
+        map(accessor(1), Array{Int}[[1,2], [3,4], [5,6]]) => [1,3,5]
+    end
+
+    @fact "juxt" begin
+        juxt(iseven, isodd)(2) => [true, false]
+    end
+
+    @fact "iterate, iterateuntil" begin
+        iterate(5, inc, 0) => [1, 2, 3, 4, 5]
+        iterateuntil((x) -> x > 5, inc, 0) => [1, 2, 3, 4, 5]
+    end
+
+    @fact "reductions" begin
+        reductions(+, 0, [1, 2, 3, 4, 5]) => [0, 1, 3, 6, 10, 15]
+        reductions(+, [1, 2, 3, 4, 5]) => [1, 3, 6, 10, 15]
+        reductions(+, []) => []
+    end
+
+end
+
+@facts "Higher order functions" begin
+
+    @fact "best" begin
+        best(>, [1, 3, 2, 5, 43, 76, 4]) => 76
+    end
+
+    @fact "fnothing" begin
+        add3 = fnothing((x, y, z) -> x + y + z, 0, 0, 0)
+        add3(1, 2, 3) => 6
+        add3(1, 2, false) => 3
+        add3(false, 2, 3) => 5
+        add3(1, false, 3) => 4
+        add3(false, false, 3) => 3
+    end
+
+    @fact "lots of curry" begin
+        curry2((x, y) -> x + y)(1)(1) => 2
+        curry3((x, y, z) -> x + y + z)(1)(1)(1) => 3
+        curry4((x, y, z, t) -> x + y + z + t)(1)(1)(1)(1) => 4
+
+        curry2(string)("foo")("bar") => "barfoo"
+        curry3(string)("foo")("bar")("baz") => "bazbarfoo"
+    end
+
+    @fact "gt, gte, lt, lte, eq" begin
+        2 => not(gt(2))
+        2 => gte(2)
+        2 => gt(1)
+        2 => not(lt(2))
+        2 => lte(2)
+        1 => lt(2)
+        2 => not(eq(1))
+        2 => eq(2)
+    end
+
+    @fact "trampoline" begin
+        even(n) = n == 0 ? true  : @jump odd(n - 1)
+        odd(n)  = n == 0 ? false : @jump even(n - 1)
+
+        trampoline(even, 1000000) => true
+    end
+
+end
